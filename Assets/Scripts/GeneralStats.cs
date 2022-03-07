f046bee7631aadea0f7e839050738a515b71345e
Miller-Anthony <54007860+Miller-Anthony@users.noreply.github.com>
2022-03-07 14:37:22 -0800

    General Stats
    Added gold to the general stats

--- Assets/Scripts/GeneralStats.cs
+++ Assets/Scripts/GeneralStats.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GeneralStats : MonoBehaviour
 {
+    private int gold;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gold = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Add a given amount of gold
+    public void AddGold(int value)
+    {
+        gold += value;
+    }
+
+    // Subtract a gien amount of gold
+    public void SubtractGold(int value)
+    {
+        gold -= value;
+    }
+
+    public int CheckGold()
+    {
+        return gold;
     }
 }
