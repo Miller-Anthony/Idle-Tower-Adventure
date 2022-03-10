167085410770801cf207de61a190c42972624e47
Miller-Anthony <miller.anthony.a@gmail.com>
2022-03-09 23:25:44 -0800

    Update Scripts
    Update Basic Spripts for the NPC's

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
+    [SerializeField] NPCStats stats;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class NPCMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //move NPC
+        //transform.position += transform.forward * stats.GetSpeed() * Time.deltaTime;
     }
 }
--- Assets/Scripts/NPCStats.cs
+++ Assets/Scripts/NPCStats.cs
@@ -4,22 +4,166 @@ using UnityEngine;
 
 public class NPCStats : MonoBehaviour
 {
-    public int level;
-    public int strength;
-    public int health;
-    public float speed;
-    public float spawn;
-    public int gold;
+    [SerializeField] int level;
+    [SerializeField] int strength;
+    [SerializeField] int health;
+    [SerializeField] float speed;
+    [SerializeField] float spawn;
+    [SerializeField] int gold;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Returns the level of the NPC
+    public int GetLevel()
+    {
+        return level;
+    }
+
+    // Set the level of the NPC to something specific
+    public void SetLevel(int correction)
+    {
+        level = correction;
+    }
+
+    // Add an amount of levels to the NPC
+    public void AddLevel(int addition)
+    {
+        level += addition;
+    }
+
+    // Subtract an amount of levels to the NPC
+    public void SubtractLevel(int subtraction)
+    {
+        level -= subtraction;
+    }
+
+    // Returns the strength of the NPC
+    public int GetStrength()
+    {
+        return strength;
+    }
+
+    // Set the strength of the NPC to something specific
+    public void SetStrength(int correction)
+    {
+        strength = correction;
+    }
+
+    // Add an amount of strength to the NPC
+    public void AddStrength(int addition)
+    {
+        strength += addition;
+    }
+
+    // Subtract an amount of strength to the NPC
+    public void SubtractStrength(int subtraction)
+    {
+        strength -= subtraction;
+    }
+
+    // Returns the health of the NPC
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    // Set the health of the NPC to something specific
+    public void SetHealth(int correction)
+    {
+        health = correction;
+    }
+
+    // Add an amount of health to the NPC
+    public void AddHealth(int addition)
+    {
+        health += addition;
+    }
+
+    // Subtract an amount of health to the NPC
+    public void SubtractHealth(int subtraction)
+    {
+        health -= subtraction;
+    }
+
+    // Returns the speed of the NPC
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    // Set the speed of the NPC to something specific
+    public void SetSpeed(float correction)
+    {
+        speed = correction;
+    }
+
+    // Add an amount of speed to the NPC
+    public void AddSpeed(float addition)
+    {
+        speed += addition;
+    }
+
+    // Subtract an amount of speed to the NPC
+    public void SubtractSpeed(float subtraction)
+    {
+        speed -= subtraction;
+    }
+
+    // Returns the spawn of the NPC
+    public float GetSpawn()
+    {
+        return spawn;
+    }
+
+    // Set the spawn of the NPC to something specific
+    public void SetSpawn(float correction)
+    {
+        spawn = correction;
+    }
+
+    // Add an amount of sapwn to the NPC
+    public void AddSpawn(float addition)
+    {
+        spawn += addition;
+    }
+
+    // Subtract an amount of spawn to the NPC
+    public void SubtractSpawn(float subtraction)
+    {
+        spawn -= subtraction;
+    }
+
+    // Returns the gold of the NPC
+    public int GetGold()
+    {
+        return gold;
+    }
+
+    // Set the gold of the NPC to something specific
+    public void SetGold(int correction)
+    {
+        gold = correction;
+    }
+
+    // Add an amount of gold to the NPC
+    public void AddGold(int addition)
+    {
+        gold += addition;
+    }
+
+    // Subtract an amount of gold to the NPC
+    public void SubtractGold(int subtraction)
+    {
+        gold -= subtraction;
     }
 }
