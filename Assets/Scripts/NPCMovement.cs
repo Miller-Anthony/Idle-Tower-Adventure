bfccd2d09d3f80ccf57d3e140a7cf0cb8d353358
Miller-Anthony <54007860+Miller-Anthony@users.noreply.github.com>
2022-03-14 16:33:09 -0700

    Click to Spawn
    Made it so clicking will spawn a warrior. the warrior will move the proper direction of the floor it is spawned on

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class NPCMovement : MonoBehaviour
 {
     [SerializeField] NPCStats stats;
-
+    [SerializeField] bool moveLeft;
     
 
     // Start is called before the first frame update
@@ -17,7 +17,25 @@ public class NPCMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //move NPC
-        gameObject.transform.position += -transform.right * stats.GetSpeed() * Time.deltaTime;
+        //move NPC left if odd floor
+        if(moveLeft)
+        {
+            gameObject.transform.position += -transform.right * stats.GetSpeed() * Time.deltaTime;
+        }
+        else
+        {
+            gameObject.transform.position += transform.right * stats.GetSpeed() * Time.deltaTime;
+        }
+        //move NPC right if even floor
+    }
+
+    public void SetMoveLeft(bool val)
+    {
+        moveLeft = val;
+    }
+
+    public bool GetMoveLeft()
+    {
+        return moveLeft;
     }
 }
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    [SerializeField] int floor = 0;
+    [SerializeField] float spawnX;
+    [SerializeField] float spawnY;
+    private Vector3 position;
+    [SerializeField] GameObject warrior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create the position the object should be spawned at
+        position = new Vector3(spawnX, spawnY, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn a warrior
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject holder = Instantiate(warrior);
+            if (floor % 2 == 0)
+            {
+                holder.GetComponent<NPCMovement>().SetMoveLeft(false);
+            }
+            else
+            {
+                holder.GetComponent<NPCMovement>().SetMoveLeft(true);
+            }
+            
+            
+            holder.transform.position = position;
+        }
+        
+    }
+
+    // Set the floor number
+    public void SetFloor(int val)
+    {
+        floor = val;
+    }
+
+    // returns the floor number
+    public int GetFloor()
+    {
+        return floor;
+    }
+}
