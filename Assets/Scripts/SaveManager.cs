7dd1ea4fe34f23c36913bd0ef7b2d7d1f3079b0d
Miller-Anthony <54007860+Miller-Anthony@users.noreply.github.com>
2022-05-04 23:13:21 -0700

    Save Manager
    added variables to be saved to the save manager

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
+    //places to get save data from
+
+    //data to save and load
+    private int highestFloor;
+    private int clearedFloors;
+    private int gold;
+    private int adventurerLevel;
+    private int adventurerCount;
+    private int clearedFloorLevel;
+    private int fighterLevels;
+    private int barbarianLevel;
+
     // Start is called before the first frame update
     void Start()
     {
